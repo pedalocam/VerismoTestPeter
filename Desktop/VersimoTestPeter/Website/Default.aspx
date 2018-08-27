a5fb3ad288f7f1be6abff518d09d44c8450fdef5
Peter Camerini <Camerini.peter@gmail.com>
2018-08-27 17:49:17 +0200

    First commit

---
+++ Desktop/VersimoTestPeter/Website/Default.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" MasterPageFile="Default.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="VerismoTest.Website.Default" %>
+
+<asp:Content ContentPlaceHolderID="cphContent1" runat="server">
+
+
+
+
+    <div class="container">
+        <div class="page-header">
+            <h1>
+                <img src="style/logo.png" alt="" /></h1>
+        </div>
+        <h3>Lista av alla "Tasks"</h3>
+        <p>Det ska gå att lägga till nya, ändra samt ta bort.</p>
+        <p>Inget ändras skarpt innan man har tryckt på "Spara".</p>
+        <div class="col-sx-12">
+            &nbsp;
+        </div>
+        <asp:GridView ID="gvData"
+            runat="server"
+            AutoGenerateColumns="False"
+            ShowFooter="True"
+            DataKeyNames="Id"
+            ShowHeaderWhenEmpty="True"
+            CellPadding="7"
+            GridLines="None"
+            OnRowEditing="gvData_RowEditing1"
+            OnRowUpdating="gvData_RowUpdating1"
+            OnRowCancelingEdit="gvData_RowCancelingEdit1"
+            OnRowDeleting="gvData_RowDeleting">
+
+            <EditRowStyle BackColor="#fffffff" />
+            <FooterStyle BackColor="#fffffff" Font-Bold="True" ForeColor="Black" BorderStyle="None" />
+            <HeaderStyle BackColor="#fffffff" Font-Bold="True" ForeColor="Black" />
+            <PagerStyle BackColor="#fffffff" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#fffffff" />
+            <SelectedRowStyle BackColor="#fffffff" Font-Bold="True" ForeColor="#fffffff" />
+            <SortedAscendingCellStyle BackColor="#fffffff" />
+            <SortedAscendingHeaderStyle BackColor="#fffffff" />
+            <SortedDescendingCellStyle BackColor="#fffffff" />
+            <SortedDescendingHeaderStyle BackColor="#fffffff" />
+
+            <Columns>
+                <asp:TemplateField HeaderText="Name">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Name") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtName" Text='<%# Eval("Name") %>' runat="server" CssClass="form-control input-sm" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtNameFooter" Text='<%# Eval("Name") %>' runat="server" CssClass="form-control input-sm" />
+                        <asp:PlaceHolder ID="PlaceHolder1" runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="DueDate">
+                    <ItemTemplate>
+                        <asp:Label Text='<%#Eval("DueDate") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtDueDate" Text='<%#Eval("DueDate") %>' runat="server" CssClass="form-control input-sm" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtDueDateFooter" Text='<%#Eval("DueDate") %>' runat="server" CssClass="form-control input-sm"/>
+                        <asp:PlaceHolder ID="PlaceHolder2" runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Description">
+                    <ItemTemplate>
+                        <asp:Label Text='<%#Eval("Description") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtDescription" Text='<%#Eval("Description") %>' runat="server" CssClass="form-control input-sm" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtDescriptionFooter" Text='<%#Eval("Description") %>' runat="server" CssClass="form-control input-sm" />
+                        <asp:PlaceHolder ID="PlaceHolder3" runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Button runat="server" CommandName="Edit" ToolTip="Edit" Text="Edit" class="btn btn-success btn-sm" />
+                        <asp:Button runat="server" CommandName="Delete" ToolTip="Delete" Text="X" class="btn btn-danger btn-sm" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:Button runat="server" CommandName="Update" ToolTip="Update" Text="Save" class="btn btn-success btn-sm" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                    </FooterTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <div class="col-md-6 text-center">
+            <asp:Button runat="server" Text="Save" class="btn btn-success btn-sm" OnClick="Save_Click" />
+        </div>
+        <br />
+        <asp:Label ID="lblSuccessMessage" Text="" runat="server" ForeColor="Green" />
+        <br />
+        <asp:Label ID="lblErrorMessage" Text="" runat="server" ForeColor="Red" />
+    </div>
+</asp:Content>
---
+++ Desktop/VersimoTestPeter/Website/Default.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VerismoTest.BLL;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace VerismoTest.Website
+{
+    public partial class Default : PageBaseClass
+    {
+
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\Peter\Desktop\VerismoTest-master - kopia\Website\App_Data\Database.mdf';Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridview();
+            }
+        }
+
+        void PopulateGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM DataEntry", sqlCon);
+                sqlDa.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvData.DataSource = dtbl;
+                gvData.DataBind();
+            }
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvData.DataSource = dtbl;
+                gvData.DataBind();
+                gvData.Rows[0].Cells.Clear();
+                gvData.Rows[0].Cells.Add(new TableCell());
+                gvData.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvData.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+        }
+
+        protected void gvData_RowEditing1(object sender, GridViewEditEventArgs e)
+        {
+            gvData.EditIndex = e.NewEditIndex;
+            PopulateGridview();
+        }
+
+        protected void gvData_RowCancelingEdit1(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvData.EditIndex = -1;
+            PopulateGridview();
+        }
+
+        protected void gvData_RowUpdating1(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE DataEntry SET Name=@Name,DueDate=@DueDate,Description=@Description WHERE Id = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@Name", (gvData.Rows[e.RowIndex].FindControl("txtName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@DueDate", (gvData.Rows[e.RowIndex].FindControl("txtDueDate") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Description", (gvData.Rows[e.RowIndex].FindControl("txtDescription") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvData.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvData.EditIndex = -1;
+                    PopulateGridview();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvData_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM DataEntry WHERE Id = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvData.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridview();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        // Jag ville skapa dynamiska textlådor som skapas med "Add Row" knappen för att sen spara allt med 
+        // "Save" knappen. Lyckades inte få detta att funka som jag ville så struntade i "Add Row" knappen då 
+        // "Save" knappen har samma funktion.
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+
+                sqlCon.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * from DataEntry", sqlCon);
+                DataTable table = new DataTable();
+                string query = "INSERT INTO DataEntry (Name,DueDate,Description) VALUES (@Name,@DueDate,@Description)";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                adapter.Fill(table);
+                sqlCmd.Parameters.AddWithValue("@Name", (gvData.FooterRow.FindControl("txtNameFooter") as TextBox).Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@DueDate", (gvData.FooterRow.FindControl("txtDueDateFooter") as TextBox).Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Description", (gvData.FooterRow.FindControl("txtDescriptionFooter") as TextBox).Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                PopulateGridview();
+                sqlCon.Close();
+
+            }
+        }
+    }
+}
